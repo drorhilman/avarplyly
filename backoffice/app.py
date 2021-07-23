c7dd4dcd53bf9f229944343291d45e778adb8c34
Dror Hilman <drorh@trellis.ai>
2021-07-23 14:20:36 +0300

    open in a new tab the jakyll server

--- backoffice/app.py
+++ backoffice/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, send_from_directory, jsonify, request
-import os, glob, yaml
+import os, glob, yaml, subprocess, builtins
+
+builtins.proc = ''
+
 
 
 
@@ -14,9 +17,13 @@ def load_post(filename, return_content = False):
         return js
         
 
+def start_local_jekyll_server():
+    builtins.proc = subprocess.Popen(["bundle", "exec","jekyll", "serve"], cwd="../")
+    return str(builtins.proc)
 
 # -------------------------- API -------------------------
 app = Flask(__name__, static_url_path='')
+
 
 @app.route("/")
 def post_editor():
@@ -47,12 +54,35 @@ def get_post_data(filepath):
         filepath = '../' + filepath
     return load_post(filepath, return_content=True)
 
+@app.route('/get_assets_list')
+def get_assets_list():
+    assets = glob.glob('../assets/*.*')
+    return jsonify(assets)
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    try:
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            filename = os.path.join('../assets/', uploaded_file.filename)
+            print(filename)
+            if os.path.isfile(filename):
+                return jsonify({'status': 'exist', 'reason' : 'filename already exist'})
+            else:
+                uploaded_file.save(filename)
+            return jsonify({'status': 'ok'})
+    except:
+        return jsonify({'status': 'error', 'reason' : 'no file'})
 
 
+@app.route('/start_jakyll_server')
+def start_jakyll():
+    return jsonify({'process': start_local_jekyll_server(), 'address': 'http://localhost:4000'})
+    
 
 #=============================================================
 if __name__ == '__main__':
-    episodes = glob.glob('../_episodes/*.*')
-    for episode in episodes:
-        content = load_post(episode, return_content=True)
-        print(content)
+    # episodes = glob.glob('../_episodes/*.*')
+    # for episode in episodes:
+    #     content = load_post(episode, return_content=True)
+    print(start_local_jekyll_server())
